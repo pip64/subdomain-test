015c66213e913c260c5c9f3805da95df3c36f3ae
pip64 <pip64b@icloud.com>
2025-07-22 13:16:38 +0700

    Add fetch user

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,50 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { useEffect, useState } from 'preact/hooks';
+
+const getSubdomain = () => {
+  const hostname = window.location.hostname;
+  const parts = hostname.split('.');
+  if (parts.length > 2) {
+    return parts[0];
+  }
+  return null;
+};
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const subdomain = getSubdomain();
+
+  async function getUser() {
+    try {
+      const response = await fetch("https://api.slipe.fun/v2/user/" + (subdomain || "slipe"));
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      setError(error.message || String(error));
+    }
+  }
+
+
+  useEffect(() => {
+    getUser()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      {user && (
+        <>
+          <img src={`https://cdn.slipe.fun/avatars/${user?.avatar}`} width={100} height={100} />
+          <h1>{user?.nickname || user?.username}</h1>
+          <h2>{user?.description || "No description"}</h2>
+        </>
+      )}
+      {error && (
+        {error}
+      )}
     </>
   )
 }
